b378c3cf0d92f242fbaf6dd37b269378a782df87
JayamaniOgss <ogss.jayamanikandan@gmail.com>
2024-01-19 12:20:13 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+function App() {
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
